887899b2929b1821460f1a11262a288e0023025a
Oleg <garrbuzz@gmail.com>
2019-07-03 15:30:33 +0300

    office

--- www/js/script.js
+++ www/js/script.js
@@ -1,24 +1,22 @@
-let burg = document.querySelector('#burger-btn');
-let backBtn = document.querySelector('#burger-back');
-let burgMenu = document.querySelector('#burger-menu');
-let links = document.querySelectorAll('#burger-menu a');
-for (let i = 0; i<links.length; i++) {
-	links[i].onclick = function(){
-		burg.classList.remove('hide');
-		burgMenu.classList.add('hide');
+window.onload = function(){
+// radio
+	let startBtn = document.querySelector('#play-stop');
+	var playStatus = 'stop';
+	var audio = new Audio('http://de1.internet-radio.com:8277/stream?type=http&amp;nocache=12');
+	startBtn.onclick = function(){
+		if (playStatus ==='stop') {
+			console.log(playStatus);
+			startBtn.innerHTML = "<img  src=\"../img/pause.png\" alt=\"\">";
+			playStatus ='play';
+			audio.play();
+			
+		} else if (playStatus ==='play'){
+			startBtn.innerHTML = "<img  src=\"../img/play.png\" alt=\"\">";
+			audio.pause();
+			playStatus ='stop';
+		}
+		
 	}
-}
-burg.onclick = function(){
-	burg.classList.add('hide');
-	burgMenu.classList.remove('hide');
-	burgMenu.classList.add('burg-wrap-start');
-	function test(){
-		burgMenu.classList.add('burger-wrap');
-	}
-	setTimeout(test, 20);
-	
-}
-backBtn.onclick = function(){
-	burg.classList.remove('hide');
-	burgMenu.classList.add('hide');
-}
+// radio end
+
+} 
